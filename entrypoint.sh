8ac5661cf189c1ff67a8f7d10c3a6c296b1c684b
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2023-04-03 17:35:45 +1000

    update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,8 @@ if [ -n "$INPUT_IGNORE" ]; then
 fi
 
 if [ "$INPUT_PR_COMMENT" = true ]; then
-    output="$(markdownlint --config $INPUT_CONFIG;return)"
+    output="$(markdownlint --config $INPUT_CONFIG $INPUT_FILES;return)"
+    output="${output//$'\r'/'%0D'}"
     echo 'MARKDOWNLINT_OUTPUT<<EOF' >> $GITHUB_OUTPUT
     echo -e $output >> $GITHUB_OUTPUT
     echo 'EOF' >> $GITHUB_OUTPUT
